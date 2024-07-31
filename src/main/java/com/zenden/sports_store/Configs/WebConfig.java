4311d353a22f3827f22bb913f7967bfb1399cb6b
Zenden <52536490+Zenden1982@users.noreply.github.com>
2024-07-31 20:40:03 +0300

    fix

--- src/main/java/com/zenden/sports_store/Configs/WebConfig.java
+++ src/main/java/com/zenden/sports_store/Configs/WebConfig.java
@@ -1,9 +1,12 @@
 package com.zenden.sports_store.Configs;
 
+import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
-public class WebConfig implements WebMvcConfigurer{
+@Configuration
+public class WebConfig implements WebMvcConfigurer  {
+
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
