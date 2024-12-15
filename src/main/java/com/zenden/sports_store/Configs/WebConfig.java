90bb0aa689d2dd3ef419b006da5297997f283e60
Zenden1982 <nikita_putilin@bk.ru>
2024-12-15 22:42:42 +0300

    final 3.0

--- src/main/java/com/zenden/sports_store/Configs/WebConfig.java
+++ src/main/java/com/zenden/sports_store/Configs/WebConfig.java
@@ -1,6 +1,10 @@
 package com.zenden.sports_store.Configs;
 
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+import org.springframework.web.filter.CorsFilter;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
@@ -11,11 +15,22 @@ public class WebConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(@NotNull CorsRegistry registry) {
         registry.addMapping("/**")
-                .allowedOrigins("http://localhost:80")
                 .allowedOrigins("http://localhost:5173")
                 .allowedMethods("GET", "POST", "PUT", "DELETE", "HEAD")
                 .allowedHeaders("*")
                 .allowCredentials(true);
+    }
+
+    @Bean
+    public CorsFilter corsFilter() {
+        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+        CorsConfiguration config = new CorsConfiguration();
+        config.setAllowCredentials(true);
+        config.addAllowedOriginPattern("*"); // Разрешить запросы с любого источника
+        config.addAllowedHeader("*"); // Разрешить любые заголовки
+        config.addAllowedMethod("*"); // Разрешить любые методы (GET, POST, PUT, DELETE и т. д.)
+        source.registerCorsConfiguration("/**", config);
+        return new CorsFilter(source);
     }
 
 }
